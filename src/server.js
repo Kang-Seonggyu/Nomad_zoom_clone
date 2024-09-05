96238f96fe73e2b3d4fb8848f426f0502d586c11
Kang-Seonggyu <103415733+Kang-Seonggyu@users.noreply.github.com>
2024-09-05 13:05:17 +0900

    Feat: Front setup

--- src/server.js
+++ src/server.js
@@ -5,6 +5,10 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 
-console.log('Hello !');
+app.use('/public', express.static(__dirname + '/public'));
 
-app.listen(4000);
+app.get('/', (req, res) => res.render('home'));
+
+const handleListen = () => console.log('Listening on http://localhost:4000');
+
+app.listen(4000, handleListen);
